930aff891c563c6b8fb74a46356b42dd9bc3a0c5
Adisak <adisak0661013@gmail.com>
2021-02-22 17:08:40 +0700

    Update lab1

--- lab1.test-connect-node/index.js
+++ lab1.test-connect-node/index.js
@@ -2,7 +2,7 @@ const algosdk = require('algosdk');
 
 const algodToken = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
 const algodServer = 'http://localhost';
-const algodPort = 4001;
+const algodPort = 4002;
 
 async function start() {
     const algodClient = new algosdk.Algodv2(algodToken, algodServer, algodPort);
@@ -10,6 +10,10 @@ async function start() {
     const status = await algodClient.status().do();
 
     console.log('Algorand network status: %o', status);
+
+    const params = await algodClient.getTransactionParams().do();
+
+    console.log('Algorand suggested parameters: %o', params);
 }
 
 start();
