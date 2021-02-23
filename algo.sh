38434ef1378b8329d2e304c4ccdc9191384dfccd
Adisak <adisak0661013@gmail.com>
2021-02-23 14:16:13 +0700

    Add script for setup lab

--- algo.sh
+++ algo.sh
@@ -4,20 +4,22 @@ Help()
 {
    # Display Help
    echo "Add a description of the script functions here."
-   echo
-   echo "lab options:"
+   echo "command options:"
+   echo "setup  Create are accounts [Bob, Aliza]."
    echo "lab1   Connection to node."
    echo "lab2   Create account."
    echo
 }
 
+COMMAND="${1:-none}"
 
-LAB="${1:-none}"
-
-if [[ "$LAB" == "lab1" ]]
+if [[ "$COMMAND" == "setup" ]]
+then
+    cd account && node index.js
+elif [[ "$COMMAND" == "lab1" ]]
 then
     cd lab1.connect-node && node index.js
-elif [[ "$LAB" == "lab2" ]]
+elif [[ "$COMMAND" == "lab2" ]]
 then
     cd lab2.create-account && node index.js
 else
