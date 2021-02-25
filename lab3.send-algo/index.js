0d14892531c81898ef8e3601c6f8ddd6f34dbabf
Adisak <adisak0661013@gmail.com>
2021-02-26 04:55:05 +0700

    Update Lab3 Refactor Code

--- lab3.send-algo/index.js
+++ lab3.send-algo/index.js
@@ -1,48 +1,58 @@
 const algo = require('../config');
 
 async function start() {
+    const checkBalance = async function (algodClient, account) {
+        console.log(account.name + ' address: ' + account.addr);
+
+        const accountInfo = await algodClient.accountInformation(account.addr).do();
+        console.log(account.name + ' balance: %d microAlgos', accountInfo.amount);
+    };
+
+    const transfer = async function (algodClient, fromAccount, toAccount, microAlgos, message) {
+        const params = await algodClient.getTransactionParams().do();
+
+        // comment out the next two lines to use suggested fee
+        params.fee = 1000;
+        params.flatFee = true;
+
+        const note = await algo.sdk.encodeObj(message);
+        const txn = await algo.sdk.makePaymentTxnWithSuggestedParams(
+            fromAccount.addr,
+            toAccount.addr,
+            microAlgos,
+            undefined,
+            note,
+            params,
+        );
+
+        const sk = new Uint8Array(bob.sk.split(','));
+        const signedTxn = await txn.signTxn(sk);
+
+        const txId = await txn.txID().toString();
+        console.log(fromAccount.name + ' signed a transaction with txID: %s', txId);
+
+        console.log(
+            `Sent ${microAlgos} microAlgos from account ${fromAccount.addr} to address ${toAccount.addr}, transaction ID: ${txId}. Fee set to ${params.fee} microAlgos`,
+        );
+
+        try {
+            await algodClient.sendRawTransaction(signedTxn).do();
+
+            console.log(
+                `Wait to 5 - 10 sec. and try to check the balance of ${fromAccount.name} and ${toAccount.name} again.`,
+            );
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
     const bob = await algo.bob();
     const aliza = await algo.aliza();
 
-    console.log('Bob address: ' + bob.addr);
+    await checkBalance(algo.client, bob);
+    await checkBalance(algo.client, aliza);
 
-    let accountInfo = await algo.client.accountInformation(bob.addr).do();
-    console.log('Bob balance: %d microAlgos', accountInfo.amount);
-
-    console.log('Aliza address: ' + aliza.addr);
-
-    accountInfo = await algo.client.accountInformation(aliza.addr).do();
-    console.log('Aliza balance: %d microAlgos', accountInfo.amount);
-
-    const params = await algo.client.getTransactionParams().do();
-
-    // comment out the next two lines to use suggested fee
-    params.fee = 1000;
-    params.flatFee = true;
-
-    const note = await algo.sdk.encodeObj('Hello World');
-    const txn = await algo.sdk.makePaymentTxnWithSuggestedParams(
-        bob.addr,
-        aliza.addr,
-        1000000,
-        undefined,
-        note,
-        params,
-    );
-
-    const sk = new Uint8Array(bob.sk.split(','));
-    const signedTxn = await txn.signTxn(sk);
-
-    const txId = await txn.txID().toString();
-    console.log('Bob signed a transaction with txID: %s', txId);
-
-    try {
-        await algo.client.sendRawTransaction(signedTxn).do();
-        console.log('Bob send 1000000 microAlgos to Aliza that has a fee of just 1000 microAlgos.');
-        console.log('Wait to 5 - 10 sec. and try to check the balance of Bob and Aliza again.');
-    } catch (err) {
-        console.log(err);
-    }
+    await transfer(algo.client, bob, aliza, 1000000, 'Hello World');
 }
 
 start();
